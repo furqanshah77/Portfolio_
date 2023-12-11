e74ed05f131a0c2a8da050061effb98ee2829559
Furqanshah77 <shahfurqan668@gmail.com>
2023-12-12 02:05:52 +0500

    Portfolio-first-section-compelete

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ function myFunction(x) {
     x.classList.toggle("change");
 
 }
+
+var typed = new Typed('#element', {
+    strings: ['<b>Developer</b>', 'FreeLancer', `Designer`, `Youtuber`, `Blogger` ],
+    typeSpeed: 50,
+    backSpeed: 10,
+    loop: true,
+  });
