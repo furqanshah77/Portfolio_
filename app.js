57e627150359994973e67c651a19cf4f9be81b62
Furqanshah77 <shahfurqan668@gmail.com>
2023-12-15 01:32:26 +0500

    First-page-compelete

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 
+
+// HamBurger
+
+
 function myFunction(x) {
     x.classList.toggle("change");
 
 }
 
+
+// Typed
+
+
 var typed = new Typed('#element', {
-    strings: ['<b>Developer</b>', 'FreeLancer', `Designer`, `Youtuber`, `Blogger` ],
+    strings: ['<b>Developer</b>', 'FreeLancer', `Designer`, `Youtuber`, `Blogger`],
     typeSpeed: 100,
     backSpeed: 10,
     loop: true,
-  });
+});
 
 
 
